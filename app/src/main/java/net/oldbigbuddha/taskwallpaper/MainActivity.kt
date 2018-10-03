cc9bf5b27d6b294767b171d12bdf0ce77ca7f863
OldBigBuddha <shinlag@outlook.jp>
2018-10-03 21:23:35 +0300

    [add] ADSL #3
    Editext -> TextInputLayout > EditText
    Add Snackbar for error about task empty
    Close #3

--- app/src/main/java/net/oldbigbuddha/taskwallpaper/MainActivity.kt
+++ app/src/main/java/net/oldbigbuddha/taskwallpaper/MainActivity.kt
@@ -2,9 +2,9 @@ package net.oldbigbuddha.taskwallpaper
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.text.TextUtils
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +14,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ArrayList<String>())
+        val adapter = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                ArrayList<String>()
+        )
 
         bt_add_task.setOnClickListener {
-            val task: String? = et_task.text.toString()
-            if ( !TextUtils.isEmpty(task) ) {
-                adapter.add(task!!)
+
+            if ( !TextUtils.isEmpty( et_task.text.toString() ) ) {
+
+                adapter.add(et_task.text.toString())
                 adapter.notifyDataSetChanged()
+
             } else {
-                Toast.makeText(this, "Empty Task", Toast.LENGTH_SHORT).show()
+
+                Snackbar.make(container, "Task is empty", Snackbar.LENGTH_SHORT).show();
+
             }
+
             et_task.setText("")
+
         }
 
         lv_task.adapter = adapter
+
     }
 }
