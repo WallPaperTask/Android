838a4f1b40220f89c1c72ac7fc8b6467f1518948
OldBigBuddha <shinlag@outlook.jp>
2018-10-03 22:01:37 +0300

    [add] Toolbar
    Change theme(Light.NoActionbar)
    Add Toolbar
    
    Close #5
    Close #4

--- app/src/main/java/net/oldbigbuddha/taskwallpaper/MainActivity.kt
+++ app/src/main/java/net/oldbigbuddha/taskwallpaper/MainActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,7 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setSupportActionBar(my_toolbar)
+        my_toolbar.inflateMenu(R.menu.main_menu)
 
         val adapter = ArrayAdapter<String>(
                 this,
@@ -39,5 +42,23 @@ class MainActivity : AppCompatActivity() {
 
         lv_task.adapter = adapter
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.action_save -> {
+            Snackbar.make(container, "Saved", Snackbar.LENGTH_SHORT).show()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
