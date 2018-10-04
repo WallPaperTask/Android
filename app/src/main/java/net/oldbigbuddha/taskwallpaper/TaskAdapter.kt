7bdabe481211bcfe70ef4cfd832a25e81682a47a
OldBigBuddha <shinlag@outlook.jp>
2018-10-04 19:05:59 +0300

    [add] Task Removable

--- app/src/main/java/net/oldbigbuddha/taskwallpaper/TaskAdapter.kt
+++ app/src/main/java/net/oldbigbuddha/taskwallpaper/TaskAdapter.kt
@@ -5,28 +5,35 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import kotlinx.android.synthetic.main.task_item.view.*
 
 class TaskAdapter(
-        private val myTasks: ArrayList<Task>,
+        private val mTasks: ArrayList<Task>,
         private val mContext: Context
 ): RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
-    override fun getItemCount(): Int = myTasks.size
+    override fun getItemCount(): Int = mTasks.size
 
     fun addTask(task: Task) {
-        myTasks.add(0, task)
+        mTasks.add(0, task)
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.task_item, parent, false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvTask.text = myTasks[position].mTaskName
+        holder.tvTask.text = mTasks[position].mTaskName
+        holder.btRemoveItem.setOnClickListener {
+            mTasks.removeAt(position)
+            notifyItemRemoved(position)
+            notifyDataSetChanged()
+        }
     }
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val tvTask = view.tv_task!!
-        val btRemoveItem = view.bt_remove_item
+        val tvTask: TextView = view.tv_task
+        val btRemoveItem: ImageButton = view.bt_remove_item
     }
 }
